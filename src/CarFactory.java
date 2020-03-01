741730061eb951c3f1d2645c869dca73891ea2f6
nikhiljain217 <55259994+nikhiljain217@users.noreply.github.com>
2020-02-29 19:48:27 -0700

    Added Simulation

--- src/CarFactory.java
+++ src/CarFactory.java
@@ -6,18 +6,18 @@ public class CarFactory {
 		
 	}
 	
-	public Car createCar(String licenseID,String name)
+	public Car createCar(String licenseID,String car)
 	{
 		if(licenseID.charAt(0)=='S'&&licenseID.charAt(1)=='U')
-			return new Suv(licenseID,name);
+			return new Suv(licenseID,car);
 		else if(licenseID.charAt(0)=='M')
-			return new Minivan(licenseID,name);
+			return new Minivan(licenseID,car);
 		else if(licenseID.charAt(0)=='E')
-			return new Economy(licenseID,name);
+			return new Economy(licenseID,car);
 		else if(licenseID.charAt(0)=='L')
-			return new Luxury(licenseID,name);
+			return new Luxury(licenseID,car);
 		else 
-			return new Standard(licenseID,name);
+			return new Standard(licenseID,car);
 			
 	}
 }
